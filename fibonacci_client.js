82820ea59066ddee7d78c9f5d8502b3752102acb
Nimish Gupta <nimish28@gmail.com>
2013-11-21 20:11:34 -0500

    logging client

--- fibonacci_client.js
+++ fibonacci_client.js
@@ -15,15 +15,46 @@ if (process.argv.length != 3)
 
 var fibonacci_server = process.argv[2];
 
+var nrequests = 0;
+var nerrors   = 0;
+
+
+var global_time = Date.now ();
+
+function _dos (req_num)
+{
+  var num = (Math.floor (Math.random ()*100) + 1)%35;
+  var url = fibonacci_server + "?num=" + num;
+
+  var t1 = Date.now ();
+  var req_time = t1 - global_time;
+
+  request (url, function (error, response, body)
+                {
+                  var time = Date.now () - t1;
+                  var ecode = error? "000" : response.statusCode;
+
+                  nerrors += (!error && (200 == response.statusCode)) ? 0 : 1;
+                  
+                  // "<request id> <start time> <response code> <fib num requested> <time>
+                  console.log (req_num + " " + req_time + " " + ecode + " " + num + " " + time);
+                });
+}
+
+
+function next_delay ()
+{
+  return 0;
+}
+
 
 function dos ()
 {
-  var url = fibonacci_server + "?num=" + ((Math.floor (Math.random ()*100) + 1)%50);
-  console.log (url);
-  request (url, function (error, response, body) {});
+  ++nrequests;
 
-  setTimeout (dos, 0);
+  _dos (nrequests);
+
+  setTimeout (dos, next_delay ());
 }
-
 
 setTimeout (dos, 0);
